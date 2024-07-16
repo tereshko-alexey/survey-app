d4a6fd1ed74162d356c6831df8861adc0a211e46
Alexey Tereshko <alexey.tereshko.uniq@gmail.com>
2024-07-16 15:26:16 +0100

    chore/ implement showing hiding clouds

--- src/modules/survey/utils.ts
+++ src/modules/survey/utils.ts
@@ -1,7 +1,15 @@
 export const makeBallActive = (ball: SVGElement) => {
   if (!ball) return;
+  hideAllClouds();
   const logo: HTMLElement | null = ball.querySelector("rect.logo");
   const cloud: HTMLElement | null = ball.querySelector("g.cloud");
   logo?.style.setProperty("fill", "url(#pattern1)");
   cloud?.style.setProperty("opacity", "1");
+};
+
+export const hideAllClouds = () => {
+  const clouds: NodeListOf<HTMLElement> = document.querySelectorAll("g.cloud");
+  clouds.forEach((cloud) => {
+    cloud.style.setProperty("opacity", "0");
+  });
 };
